79ec5880e76f4331da08829556f497103c33d2a6
Vishnu Vijayan <vishnu08ptb@gmail.com>
2022-01-11 19:22:37 +0530

    feat: chatbot

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    /* eslint-disable */
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "f4753fd9-d482-465c-9065-bed20cb5ad9c";
+
+    (function () {
+      const d = document;
+      const s = d.createElement("script");
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+    /* eslint-enable */
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
